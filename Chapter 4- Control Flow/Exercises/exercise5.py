9a7dc5c873be5a47c296955c284c8b3ca0797ad0
jeffloyd123 <jeffloyd286@gmail.com>
2023-11-17 03:57:48 +0400

    chapter 4

---
+++ Chapter 4- Control Flow/Exercises/exercise5.py
@@ -0,0 +1,25 @@
+fruits = ['grape', 'mango', 'orange', 'lichi', 'water-melon']
+
+# Define a list of favorite fruits.
+fav_fruits = ['lichi', 'mango', 'water-melon']
+
+# Check if specific fruits are in the list of favorite fruits and provide feedback.
+if 'grape' in fav_fruits:
+    # If 'grape' is in the list of favorite fruits, print a message about liking apples.
+    print("You really like grape")
+
+if 'mango' in fav_fruits:
+    # If 'mango' is in the list of favorite fruits, print a message about liking oranges.
+    print("You really like mango")
+
+if 'orange' in fav_fruits:
+    # If 'orange' is in the list of favorite fruits, print a message about liking pears.
+    print("You really like orange")
+
+if 'lichi' in fav_fruits:
+    # If 'lichi' is in the list of favorite fruits, print a message about liking watermelons.
+    print("You really like lichi")
+
+if 'water-melon' in fav_fruits:
+    # If 'water-melon' is in the list of favorite fruits, print a message about liking peaches.
+    print("You really like water-melon")
